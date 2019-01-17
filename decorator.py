20a6e8a4d3b4ff05f06f82e653a8ba8fa58f07e6
bemenst <steve.bement@becu.org>
2019-01-17 14:34:40 -0800

    finished decorator

--- decorator.py
+++ decorator.py
@@ -1,18 +1,32 @@
-def my_decorator():
+def my_decorator(text):
     def wrapper(func):
-        def wrapped(name):
-            print("Something is happening before the function is called.")
-            func(name)
-            print("Something is happening after the function is called.")
+        def wrapped(firstName, lastName):
+            print("Something is happening before the function is called. " + text)
+            func(firstName, lastName)
+            print("Something is happening after the function is called. " + text)
         return wrapped
     return wrapper
 
-@my_decorator()
+@my_decorator("parameter")
+def say_hello(firstName, lastName):
+    print("Hello " + firstName, lastName)
+
+say_hello("Bob", "Bobber")
+
+
+
+def my_decorator(func):
+    def wrapper(name):
+        print("Something is happening before the function is called.")
+        func(name)
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
 def say_hello(name):
     print("Hello " + name)
 
 say_hello("Bob")
-
 
 
 
